5f9c6c207591cd953df5745818086b902ba09409
mezealot <32149884+mezealot@users.noreply.github.com>
2020-04-11 11:53:30 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,7 +9,7 @@
 sed -i 's/192.168.1.1/192.168.90.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/project-openwrt/luci-app-koolproxyR package/luci-app-koolproxyR
-git https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 echo "src-git lienol https://github.com/Lienol/openwrt-package" >> ./feeds.conf.default
 ./scripts/feeds clean
 ./scripts/feeds update -a
